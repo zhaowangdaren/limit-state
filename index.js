6e99fbdc78937c42abe7e3dd27cf713f5ecad795
zhaoyan <zhaoyan018153@gtjas.com>
2020-05-17 15:07:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+/**
+ * 存储
+ * 例子
+ * 初始设置，某个值
+ Store.set('test', new Date().getTime())
+ // 绑定事件
+  Store.bind('test', (v) => {
+    console.log('new test value', v, Store.state)
+  })
+  setTimeout(() => {
+    console.log('set store')
+    Store.state.test = new Date().getTime()
+  }, 1000)
+  删除某一属性
+  Store.clear(key)
+  删除全部属性
+  Store.clearAll()
+ */
+class Store {
+  constructor () {
+    this.stateShadow = {}
+    this.state = {}
+    this.bindStack = {}
+  }
+
+  set (key, value) {
+    // this.state[key] = new Object(value)
+    this.stateShadow[key] = value
+    Object.defineProperty(this.state, key, {
+      get: () => { return this.stateShadow[key] },
+      set: (v) => { 
+        this.stateShadow[key] = v
+        this.dispatchChange(key)
+      },
+      enumerable: true,
+      configurable: true
+    })
+    return this
+  }
+
+  bind (key, cb) {
+    if (typeof this.bindStack[key] === 'undefined') {
+      this.bindStack[key] = []
+    }
+    this.bindStack[key].push(cb)
+    return this
+  }
+
+  dispatchChange (key) {
+    if (typeof this.bindStack[key] !== 'undefined' && this.bindStack[key].length) {
+      let len = this.bindStack[key].length
+      while (len--) {
+        this.bindStack[key][len].call(null, this.state[key])
+      }
+    }
+  }
+  
+  clear (key) {
+    delete this.state[key]
+    delete this.stateShadow[key]
+    return this
+  }
+  
+  clearAll () {
+    for (let key in this.stateShadow) {
+      delete this.state[key]
+      delete this.stateShadow[key]
+    }
+    return this
+  }
+}
+
+const store = new Store()
+
+export default store
